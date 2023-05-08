e9c682f24a4fdf1b0f86e8ced62cbaef368d020f
Andre Contreras <akd3vs@gmail.com>
2023-05-08 12:55:40 -0600

    feat(uts): added multiple test case

--- src/components/atoms/__tests__/AtomSelect.spec.ts
+++ src/components/atoms/__tests__/AtomSelect.spec.ts
@@ -7,7 +7,9 @@ import options from '@/data/productTypes'
 
 function setup(
   config: {
-    props?: {}
+    props?: {
+      multiple: boolean
+    }
   } = {}
 ) {
   const {
@@ -53,13 +55,36 @@ describe('AtomSelect', () => {
 
     const optionsElements = within(container).getAllByTestId('atom-select-option')
 
-    expect(optionsElements).toHaveLength(options.length)
+    let childrenLength = 0
+
+    options.forEach((option) => {
+      if (option.children) {
+        childrenLength += option.children.length
+      }
+    })
+
+    expect(optionsElements).toHaveLength(options.length + childrenLength)
+
+    await userEvent.click(optionsElements[2])
+
+    const selectedOptionLabel = screen.getByTestId('atom-select-selected-option')
+
+    expect(selectedOptionLabel.innerHTML).toBe(options[1].label)
+  })
+
+  it('handles correctly clicking in a group', async () => {
+    const { input, container } = setup()
+    const userEvent = user.setup()
+
+    await userEvent.click(input)
+
+    const optionsElements = within(container).getAllByTestId('atom-select-option')
 
     await userEvent.click(optionsElements[0])
 
     const selectedOptionLabel = screen.getByTestId('atom-select-selected-option')
 
-    expect(selectedOptionLabel.innerHTML).toBe('Fresh produce')
+    expect(selectedOptionLabel.innerHTML).toBe('Select option')
   })
 
   it('closes when clicking outside', async () => {
@@ -92,5 +117,29 @@ describe('AtomSelect', () => {
     const selectedOptionLabel = screen.getByTestId('atom-select-selected-option')
 
     expect(selectedOptionLabel.innerHTML).toBe('Meat')
+  })
+
+  it('handles correctly multiple selection', async () => {
+    const { input, container } = setup({
+      props: {
+        multiple: true
+      }
+    })
+    const userEvent = user.setup()
+
+    await userEvent.click(input)
+
+    const optionsElements = within(container).getAllByTestId('atom-select-option')
+
+    await userEvent.click(optionsElements[1])
+    await userEvent.click(optionsElements[2])
+
+    const selectedOptionLabel = screen.getByTestId('atom-select-selected-option')
+
+    if (options[0].children) {
+      expect(selectedOptionLabel.innerHTML).toBe(
+        `${options[0].children[0].label}, ${options[1].label}`
+      )
+    }
   })
 })
